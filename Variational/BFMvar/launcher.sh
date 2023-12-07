14e83de3fb1af5558d9023c6346c3de2ce16e356
Anna Teruzzi <ateruzzi@login01.g100.cineca.it>
2023-12-07 16:48:24 +0100

    removing unnecessary restart files

--- Variational/BFMvar/launcher.sh
+++ Variational/BFMvar/launcher.sh
@@ -4,7 +4,6 @@
 rm -f result_0001.nc
 rm -f restart_0001.nc
 rm -f restart.nc
-cp restart20191231_nudgfloat.nc restart.nc
 mkdir -p OUTNC
 
 mpiexec -n 1 python runVar.py : -n 1 eat-gotm
--- Variational/Free/launcher.sh
+++ Variational/Free/launcher.sh
@@ -3,7 +3,6 @@
 
 rm -f result.nc
 rm -f restart.nc
-cp restart20191231_nudgfloat.nc restart.nc
 
 
 eat-gotm
