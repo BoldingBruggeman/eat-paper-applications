52eb6c005012ab64e6605d2359ac8625c32a3e30
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-05-03 13:41:02 +0100

    fix test

--- .github/workflows/generate_ensemble.py
+++ .github/workflows/generate_ensemble.py
@@ -6,7 +6,7 @@ rng = np.random.default_rng()
 N = 2
 
 # Vary wind speeds (x and y components) and background mixing (minimum turbulent kinetic energy)
-gotm = eatpy.models.gotm.YAMLEnsemble("gotm.yaml", N)
+gotm = eatpy.models.gotm.YAMLEnsemble("../reference/gotm.yaml", N)
 with gotm:
     gotm["surface/u10/scale_factor"] = rng.lognormal(mean=0.0, sigma=0.2, size=N)
     gotm["surface/v10/scale_factor"] = rng.lognormal(mean=0.0, sigma=0.2, size=N)
