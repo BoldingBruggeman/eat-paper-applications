6c3dbb3d008734fd9649fd63213b75561c9bc619
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-11-18 16:05:45 +0000

    update Ensemble for latest FABM

--- Ensemble/da_bgc/run.py
+++ Ensemble/da_bgc/run.py
@@ -1,12 +1,10 @@
 import eatpy
 
 # Notes:
-# * If you are running ERGOM, replace total_chlorophyll_calculator_result with msi_ergom1_tot_chla
+# * If you are running ERGOM, replace total_chlorophyll with msi_ergom1_tot_chla
 # * A simpler example where only SST is assimilated is given in assimilate_sst.py
 
-experiment = eatpy.models.GOTM(
-    diagnostics_in_state=["total_chlorophyll_calculator_result"]
-)
+experiment = eatpy.models.GOTM(diagnostics_in_state=["total_chlorophyll"])
 
 filter = eatpy.PDAF(eatpy.pdaf.FilterType.ESTKF)
 
@@ -16,14 +14,11 @@ experiment.add_plugin(eatpy.plugins.select.Select(include=bgc_variables))
 experiment.add_plugin(eatpy.plugins.check.Finite())
 experiment.add_plugin(
     eatpy.plugins.transform.Log(
-        "total_chlorophyll_calculator_result",
-        *bgc_variables,
-        transform_obs=False,
-        minimum=1e-12
+        "total_chlorophyll", *bgc_variables, transform_obs=False, minimum=1e-12
     )
 )
 
 # If you comment out the two lines below, you run the ensemble only without assimilation
-experiment.add_observations("total_chlorophyll_calculator_result[-1]", "../observations/cci_chl.dat")
+experiment.add_observations("total_chlorophyll[-1]", "../observations/cci_chl.dat")
 
 experiment.run(filter)
